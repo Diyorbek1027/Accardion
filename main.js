3cd627e15ce31d068bc17147a6d68973228319c5
Diyorbek1027 <80567853+Diyorbek1027@users.noreply.github.com>
2022-01-13 20:46:52 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+const accardion = document.getElementsByClassName('contentBx');
+
+for(i=0;i<accardion.length;i++){
+    accardion[i].addEventListener('click',function(){
+        this.classList.toggle('active')
+    })
+}
